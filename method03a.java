a46a1ecabb8f5c6c7ecbbbf23336e92c0055da32
Pongpanot <pongpanotpun47@gmail.com>
2024-08-16 11:19:46 +0700

    Add files via upload

---
+++ method03a.java
@@ -0,0 +1,9 @@
+public class method03a {
+    public static void main(String[] args) {
+        String[]name={"Tom","Jennie","Anna"};
+        for(String i  : name){
+            System.out.println(i + " is a nice friend");
+        
+}
+}
+}
