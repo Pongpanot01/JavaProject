37500cb029fe2d35bd48ea946c51c136410796b7
Pongpanot <pongpanotpun47@gmail.com>
2024-08-16 10:12:40 +0700

    Add files via upload

---
+++ java001.java
@@ -0,0 +1,9 @@
+public class java001 {
+
+    public static void main(String[] args) {
+        String[]cars= {"Volvo","BMW","Ford","Mazda"};
+        cars[0]="Mazda";
+        System.out.println(cars[0]);
+
+    }
+}
---
+++ java003.java
@@ -0,0 +1,10 @@
+public class java003 {
+    public static void main(String[] args) {
+        String[]cars= {"Toyota","Honda","Isuzu","MG"};
+        int i = 0;
+        while ( i < cars.length){
+        System.out.println(cars[i]);
+        i++;
+    }
+}
+}
