fe7c4fe774a33362a9efb4d147797a41fcf0cd6f
Pongpanot <pongpanotpun47@gmail.com>
2024-08-30 11:16:23 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Student student = new Student("John Wick", "Biology", 35);
+        student.displayInfo();
+
+        //OOP concept
+        //1.Abstraction นามธรรม
+        //2.Inheritance การสืบทอด
+        //3.Polymorphism การพ้องรูป
+        //4.Encapsulation การห่อหุ้ม
+    }
+}
