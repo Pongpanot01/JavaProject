5690da8083827f13f93b0208a827c9755e1d04c4
Pongpanot <pongpanotpun47@gmail.com>
2024-09-06 09:43:10 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Dog dog = new Dog("OSE");
-        dog.sound();
-        Fish fish = new Fish("Nemo");
-        fish.move();
+        Car myCar = new Car("Toyota", 2002);
+        myCar.displayInfo();
+
+        myCar.setBrand("Honda");
+        myCar.setYear(1999);
+
+        myCar.displayInfo();
     }
 }
